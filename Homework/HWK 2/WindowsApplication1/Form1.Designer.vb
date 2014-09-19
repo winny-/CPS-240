25c8fa4e94583ab89fe5a7d26683439dd842ef61
Winston Weinert <winston@ml1.net>
2014-09-19 01:23:05 -0500

    HWK 2

---
+++ Homework/HWK 2/WindowsApplication1/Form1.Designer.vb
@@ -0,0 +1,155 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtOutput = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.txtOutputPercentage = New System.Windows.Forms.TextBox()
+        Me.txtInputDecimal = New System.Windows.Forms.TextBox()
+        Me.btnGo = New System.Windows.Forms.Button()
+        Me.txtInput = New System.Windows.Forms.TextBox()
+        Me.lblOutput = New System.Windows.Forms.Label()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.btnClearList = New System.Windows.Forms.Button()
+        Me.lstResults = New System.Windows.Forms.ListBox()
+        Me.SuspendLayout()
+        '
+        'txtOutput
+        '
+        Me.txtOutput.Location = New System.Drawing.Point(12, 12)
+        Me.txtOutput.Multiline = True
+        Me.txtOutput.Name = "txtOutput"
+        Me.txtOutput.ReadOnly = True
+        Me.txtOutput.Size = New System.Drawing.Size(149, 65)
+        Me.txtOutput.TabIndex = 0
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(12, 83)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Go 0"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'txtOutputPercentage
+        '
+        Me.txtOutputPercentage.Location = New System.Drawing.Point(167, 38)
+        Me.txtOutputPercentage.Name = "txtOutputPercentage"
+        Me.txtOutputPercentage.ReadOnly = True
+        Me.txtOutputPercentage.Size = New System.Drawing.Size(100, 20)
+        Me.txtOutputPercentage.TabIndex = 2
+        '
+        'txtInputDecimal
+        '
+        Me.txtInputDecimal.Location = New System.Drawing.Point(167, 12)
+        Me.txtInputDecimal.Name = "txtInputDecimal"
+        Me.txtInputDecimal.Size = New System.Drawing.Size(100, 20)
+        Me.txtInputDecimal.TabIndex = 3
+        '
+        'btnGo
+        '
+        Me.btnGo.Location = New System.Drawing.Point(167, 64)
+        Me.btnGo.Name = "btnGo"
+        Me.btnGo.Size = New System.Drawing.Size(75, 23)
+        Me.btnGo.TabIndex = 4
+        Me.btnGo.Text = "Go dec"
+        Me.btnGo.UseVisualStyleBackColor = True
+        '
+        'txtInput
+        '
+        Me.txtInput.Location = New System.Drawing.Point(103, 130)
+        Me.txtInput.Name = "txtInput"
+        Me.txtInput.Size = New System.Drawing.Size(100, 20)
+        Me.txtInput.TabIndex = 5
+        Me.txtInput.Text = "euoeuoaeu"
+        '
+        'lblOutput
+        '
+        Me.lblOutput.AutoSize = True
+        Me.lblOutput.Location = New System.Drawing.Point(131, 157)
+        Me.lblOutput.Name = "lblOutput"
+        Me.lblOutput.Size = New System.Drawing.Size(39, 13)
+        Me.lblOutput.TabIndex = 6
+        Me.lblOutput.Text = "Label1"
+        '
+        'btnClear
+        '
+        Me.btnClear.Location = New System.Drawing.Point(116, 173)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(75, 23)
+        Me.btnClear.TabIndex = 7
+        Me.btnClear.Text = "Clear"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'btnClearList
+        '
+        Me.btnClearList.Location = New System.Drawing.Point(288, 191)
+        Me.btnClearList.Name = "btnClearList"
+        Me.btnClearList.Size = New System.Drawing.Size(75, 23)
+        Me.btnClearList.TabIndex = 8
+        Me.btnClearList.Text = "Clear List"
+        Me.btnClearList.UseVisualStyleBackColor = True
+        '
+        'lstResults
+        '
+        Me.lstResults.FormattingEnabled = True
+        Me.lstResults.Items.AddRange(New Object() {"hello world", "goodbye world"})
+        Me.lstResults.Location = New System.Drawing.Point(259, 75)
+        Me.lstResults.Name = "lstResults"
+        Me.lstResults.Size = New System.Drawing.Size(120, 95)
+        Me.lstResults.TabIndex = 9
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(399, 248)
+        Me.Controls.Add(Me.lstResults)
+        Me.Controls.Add(Me.btnClearList)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.lblOutput)
+        Me.Controls.Add(Me.txtInput)
+        Me.Controls.Add(Me.btnGo)
+        Me.Controls.Add(Me.txtInputDecimal)
+        Me.Controls.Add(Me.txtOutputPercentage)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.txtOutput)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents txtOutput As System.Windows.Forms.TextBox
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents txtOutputPercentage As System.Windows.Forms.TextBox
+    Friend WithEvents txtInputDecimal As System.Windows.Forms.TextBox
+    Friend WithEvents btnGo As System.Windows.Forms.Button
+    Friend WithEvents txtInput As System.Windows.Forms.TextBox
+    Friend WithEvents lblOutput As System.Windows.Forms.Label
+    Friend WithEvents btnClear As System.Windows.Forms.Button
+    Friend WithEvents btnClearList As System.Windows.Forms.Button
+    Friend WithEvents lstResults As System.Windows.Forms.ListBox
+
+End Class
---
+++ Homework/HWK 2/WindowsApplication1/Form1.vb
@@ -0,0 +1,46 @@
+﻿Public Class Form1
+
+    Private Const DISCOUNT As Decimal = 0.039D
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim decCost As Decimal = 9.99D
+        txtOutput.Text = String.Format(
+            "I will send you {0:C}{1}for the software",
+            decCost,
+            ControlChars.CrLf
+        )
+    End Sub
+
+    Private Sub btnGo_Click(sender As Object, e As EventArgs) Handles btnGo.Click
+        Dim n As Decimal
+
+        txtOutputPercentage.Clear()
+
+        Try
+            n = Decimal.Parse(txtInputDecimal.Text)
+        Catch ex As Exception When _
+            TypeOf ex Is FormatException OrElse TypeOf ex Is OverflowException
+
+            MessageBox.Show(text:="Enter a number please",
+                            caption:="Error")
+            Return
+        End Try
+
+        n += 0.05D
+
+        txtOutputPercentage.Text = n.ToString("P")
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        txtInput.Clear()
+        lblOutput.Text = String.Empty
+        txtInput.Focus()
+    End Sub
+
+    Private Sub btnClearList_Click(sender As Object, e As EventArgs) Handles btnClearList.Click
+        With lstResults.Items
+            .Clear()
+            .Add(DISCOUNT.ToString("P"))
+        End With
+    End Sub
+End Class
