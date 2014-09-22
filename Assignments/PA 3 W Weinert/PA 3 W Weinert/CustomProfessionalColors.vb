cd5227f40accdbefdead71639ddc8ba56edd2f31
Winston Weinert <winston@ml1.net>
2014-09-22 13:06:03 -0500

    Remove dead code & blend toolstrip colors

--- Assignments/PA 3 W Weinert/PA 3 W Weinert/CustomProfessionalColors.vb
+++ Assignments/PA 3 W Weinert/PA 3 W Weinert/CustomProfessionalColors.vb
@@ -5,37 +5,29 @@ Public Class CustomProfessionalColors
 
     Private _color As Color
 
+    'Ideally one would use Hue, Saturation, Value to blend the default Value (Relative brightness)
+    'with the _color variable. But alas .net doesn't offer that, so just do a simple average blend.
+    Public Shared Function blendColorsAverage(ByVal a As Color, ByVal b As Color) As Color
+        Dim avg = Function(x As Integer, y As Integer) (x + y) \ 2
+
+        Return Color.FromArgb(red:=avg(a.R, b.R),
+                              green:=avg(a.G, b.G),
+                              blue:=avg(a.B, b.B))
+    End Function
+
     Public Sub New(ByVal c As Color)
         _color = c
     End Sub
 
-    Public Overrides ReadOnly Property ToolStripContentPanelGradientBegin As Color
-        Get
-            Return _color
-        End Get
-    End Property
-
-    Public Overrides ReadOnly Property ToolStripGradientMiddle As Color
-        Get
-            Return MyBase.ToolStripGradientMiddle
-        End Get
-    End Property
-
-    Public Overrides ReadOnly Property ToolStripGradientEnd As Color
-        Get
-            Return MyBase.ToolStripGradientEnd
-        End Get
-    End Property
-
     Public Overrides ReadOnly Property MenuStripGradientBegin As Color
         Get
-            Return _color
+            Return blendColorsAverage(MyBase.MenuStripGradientBegin, _color)
         End Get
     End Property
 
     Public Overrides ReadOnly Property MenuStripGradientEnd As Color
         Get
-            Return MyBase.MenuStripGradientEnd
+            Return blendColorsAverage(MyBase.MenuStripGradientEnd, _color)
         End Get
     End Property
 
