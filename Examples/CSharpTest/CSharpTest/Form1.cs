15e5ecceb47fa6cf9fe307e71a3c481c09874856
Winston Weinert <winston@ml1.net>
2014-09-26 08:52:39 -0500

    Playing with C#

---
+++ Examples/CSharpTest/CSharpTest/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CSharpTest
+{
+    public partial class Form1 : Form
+    {
+        private Random r = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(
+                text: "Hello world",
+                caption: "Title!",
+                buttons: MessageBoxButtons.OK,
+                icon: MessageBoxIcon.Information
+            );
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            for (int i = 0; i < 10; i++)
+            {
+                listBox1.Items.Add(r.Next(1001));
+            }
+        }
+    }
+}
